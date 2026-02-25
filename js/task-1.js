fa80dcde11b6afd30d90307f475a622d1fb49147
MariaRadchuk <mradchuk_15@ukr.net>
2026-02-25 22:01:46 +0200

    js1

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,7 @@
+'use strict';
 function makeTransaction(quantity, pricePerDroid) {
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`
+    const totalPrice = quantity * pricePerDroid;
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
 
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
